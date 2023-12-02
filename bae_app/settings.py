c503612a777cd07e6d17f81f2ead4027387e7630
Aaron Bolton <aaron@boltonstudios.com>
2023-12-02 14:37:46 -0500

    Added dotenv

--- bae_app/settings.py
+++ bae_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 import sys
 from pathlib import Path
+from dotenv import load_dotenv
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
 
@@ -20,17 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# Load environment variables from .env file
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-os.environ["DEBUG"] = "True"
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
 "127.0.0.1,localhost").split(",")
 
-os.environ["DEVELOPMENT_MODE"] = "True"
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 # Application definition
