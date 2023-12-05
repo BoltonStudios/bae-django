e12a2802b410073d5b5d537f98492bd4a666a670
Aaron Bolton <aaron@boltonstudios.com>
2023-12-04 19:01:43 -0500

    Updated models to loose character restriction.

--- sliders/models.py
+++ sliders/models.py
@@ -10,10 +10,10 @@ class User( models.Model ):
     """
     Class to define the User table.
     """
-    instance_id     = models.CharField( primary_key=True, max_length=200 )
-    site_id         = models.CharField( unique=True, max_length=200 )
-    user_id         = models.CharField( unique=True, max_length=200 )
-    refresh_token   = models.CharField( unique=True, max_length=200 )
+    instance_id     = models.CharField( primary_key=True, max_length=255 )
+    site_id         = models.CharField( unique=True, max_length=255 )
+    user_id         = models.CharField( unique=True, max_length=255 )
+    refresh_token   = models.TextField()
     is_free         = models.BooleanField()
     created         = models.DateTimeField( auto_now_add=True )
     last_modified   = models.DateTimeField( auto_now=True )
@@ -26,14 +26,14 @@ class Extension( models.Model ):
     """
     Class to define the Extension table.
     """
-    extension_id        = models.CharField( primary_key=True, max_length=200 )
+    extension_id        = models.CharField( primary_key=True, max_length=255 )
     instance_id         = models.ForeignKey( User, on_delete=models.CASCADE )
-    before_image        = models.CharField( max_length=1000 )
-    before_label_text   = models.CharField( max_length=1000 )
-    before_alt_text     = models.CharField( max_length=1000 )
-    after_image         = models.CharField( max_length=1000 )
-    after_label_text    = models.CharField( max_length=1000 )
-    after_alt_text      = models.CharField( max_length=1000 )
+    before_image        = models.TextField()
+    before_label_text   = models.CharField( max_length=255 )
+    before_alt_text     = models.CharField( max_length=255 )
+    after_image         = models.TextField()
+    after_label_text    = models.CharField( max_length=255 )
+    after_alt_text      = models.CharField( max_length=255 )
     offset              = models.IntegerField()
     offset_float        = models.FloatField()
     is_vertical         = models.BooleanField()
